cc22f23e0243b7f2aca100ba54ee9b7df4127776
Joni0104 <joni99857622@mail.ru>
2025-08-20 15:33:31 +0300

    end hw

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,40 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FacultyService {
+    private final FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty createFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty findFaculty(Long id) {
+        Optional<Faculty> optionalFaculty = facultyRepository.findById(id);
+        return optionalFaculty.orElse(null);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        if (facultyRepository.existsById(faculty.getId())) {
+            return facultyRepository.save(faculty);
+        }
+        return null;
+    }
+
+    public void deleteFaculty(Long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public List<Faculty> getFacultiesByColor(String color) {
+        return facultyRepository.findByColor(color);
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,40 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(Long id) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        return optionalStudent.orElse(null);
+    }
+
+    public Student editStudent(Student student) {
+        if (studentRepository.existsById(student.getId())) {
+            return studentRepository.save(student);
+        }
+        return null;
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public List<Student> getStudentsByAge(int age) {
+        return studentRepository.findByAge(age);
+    }
+}
